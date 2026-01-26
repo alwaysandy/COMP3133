0b4ba872e20e5345d1231af597064e5f653a4b84
Andrew Sas <andrewdaurio@gmail.com>
2026-01-25 20:31:22 -0500

    Lab Exercise 1

--- Lab01_101462946_COMP3133/index.js
+++ Lab01_101462946_COMP3133/index.js
@@ -1,3 +1,25 @@
 const fs = require('fs');
+const csv = require('csv-parser');
+
+try {
+    fs.unlinkSync("canada.txt");
+    fs.unlinkSync("usa.txt");
+} catch {
+    console.log("Could not find files canada.txt, usa.txt");
+}
+
+fs.writeFileSync("canada.txt", "country,year,population");
+fs.writeFileSync("usa.txt", "country,year,population");
+
+fs.createReadStream('input_countries.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+        if (row["country"] === "Canada") {
+            fs.appendFileSync("canada.txt", `\n${row["country"]},${row["year"]},${row["population"]}`);
+        }
 
 
+        if (row["country"] === "United States") {
+            fs.appendFileSync("usa.txt", `\n${row["country"]},${row["year"]},${row["population"]}`);
+        }
+    });
